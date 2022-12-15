46fa4c6aaa808f79dbc8810a304470994101a0da
Magí Rull Jiménez <magirulljimenez@Magis-MacBook-Pro.local>
2022-12-15 20:04:37 +0100

    edicio de magik

--- Grafics.R
+++ Grafics.R
@@ -2,14 +2,14 @@
 window(height = 6, width = 18)
 par(mfrow = c(1, 3), las = 1, font.lab = 4, font.axis = 3, cex.lab = 1.4,
     cex.axis = 1.3)
-with(grups2122, plot(Casa2Fib2, Casa2Fib2, pch = 16, col = 1, cex = 1.5))
+with(prova_dades, plot(Quicksort, Insertionsort, pch = 16, col = 1, cex = 1.5))
 title("Grafico de dispersion")
  
 ##boxplot 
 window(width = 9)
 par(las = 1, font.lab = 4, font.axis = 3, cex.lab = 1.2,
     cex.axis = 1.1)
-boxplot(Casa2Fib2 ~ Edat, data = grups2122, pch = 16, col = 5:6,
+boxplot(Quicksort ~ Insertionsort, data = prova_dades, pch = 16, col = 5:6,
         ylab = "Quicksort")
 title("Boxplot")
 
--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
-rm out.txt
+TIMEFORMAT=%R
+rm outquick.txt outinsertion.txt
 for i in {1..100}
 do
-    echo "Caso $i" >> out.txt
+    #echo "Caso $i" >> out.txt
     ./generator > in
-    echo "Quicksort time" >> out.txt
-    { time ./quick < in; } 2>> out.txt
-    echo $'\n' >> out.txt
-    echo "Insertionsort time" >> out.txt
-    { time ./insertion < in; } 2>> out.txt
-    echo $'\n' >> out.txt
+    #echo "Quicksort time" >> out.txt
+    { time ./quick < in; } 2>> outquick.txt
+    #echo $'\n' >> out.txt
+    #echo "Insertionsort time" >> out.txt
+    { time ./insertion < in; } 2>> outinsertion.txt
+    #echo $'\n' >> out.txt
 done
