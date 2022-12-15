e6a3f5f1c6bbb37d2424d676ca15ad01c2a0d237
Magí Rull Jiménez <magirulljimenez@10-192-90-192client.eduroam.upc.edu>
2022-12-15 13:30:32 +0100

    script de R para hacer los graficos

--- Grafics.R
+++ Grafics.R
@@ -1,15 +1,18 @@
 ## Gráfico de dispersión
-quartz(height = 6, width = 18)
+window(height = 6, width = 18)
 par(mfrow = c(1, 3), las = 1, font.lab = 4, font.axis = 3, cex.lab = 1.4,
     cex.axis = 1.3)
 with(grups2122, plot(Casa2Fib2, Casa2Fib2, pch = 16, col = 1, cex = 1.5))
-
-  
-quartz(width = 9)
+title("Grafico de dispersion")
+ 
+##boxplot 
+window(width = 9)
 par(las = 1, font.lab = 4, font.axis = 3, cex.lab = 1.2,
     cex.axis = 1.1)
 boxplot(Casa2Fib2 ~ Edat, data = grups2122, pch = 16, col = 5:6,
         ylab = "Quicksort")
-title("Average salary per region")
+title("Boxplot")
 
+
+##qqplot
 
